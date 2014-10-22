b72e695ce84771e04db40ed71de465c56794367a
Odd-Henrik Aasen <oddhenrik@hotmail.com>
2014-10-22 18:03:01 +0200

    #9 Working on temp variable logic and plumbing

--- SmartDevices.indigoPlugin/Contents/Server Plugin/plugin.py
+++ SmartDevices.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -466,20 +466,20 @@ class Plugin(indigo.PluginBase):
     def _handleChangeTemperatureSensors(self, thermostatDev, sensorDev):
         self.debugLog(u"************************** handleChangeTemperatureSensors *************************************")
         self.debugLog(u"*************************** " + str(sensorDev.name) + u" **************************************")
-
-        # #Check first to see if we got a sensorValue
-        # if sensorDev.sensorValue is None and sensorDev.value is None:
-        #     #No sensor value, error wrong or not sensor
-        #     self.errorLog(u"ERROR: Sensor: " + str(sensorDev.name) + u" ,has no sensor value. Please remove sensor from list.")
-        #     return
-        #
-        # if sensorDev.sensorValue is not None:
-        #     self.debugLog(u"Sensor Device update found, sensor value = " + str(sensorDev.sensorValue))
-        #
-        # if sensorDev.value is not None:
-        #     self.debugLog(u"Sensor Variable update found, variable value = " + str(sensorDev.value))
-
         self.debugLog(u"Sensor is of type = " + str(type(sensorDev)))
+
+        if type(sensorDev) is indigo.Variable:
+            #Check first to see if we got a sensorValue
+            if sensorDev.value is None:
+                #No sensor value, error wrong or not sensor
+                self.errorLog(u"ERROR: Variable: " + str(sensorDev.name) + u" ,has no value. Please remove sensor from list.")
+                return
+        else:
+            #Check first to see if we got a sensorValue
+            if sensorDev.sensorValue is None:
+                #No sensor value, error wrong or not sensor
+                self.errorLog(u"ERROR: Sensor: " + str(sensorDev.name) + u" ,has no sensor value. Please remove sensor from list.")
+                return
 
         # Temperature sensors
         if thermostatDev.pluginProps.get("primaryTemperatureSensors", ""):
